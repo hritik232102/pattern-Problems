e1fbdb4e852d57857b177b81abd21d40ebe82275
hritik232102 <121225611+hritik232102@users.noreply.github.com>
2023-03-10 15:07:30 +0530

    Add files via upload

---
+++ conditional.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int bro_num;
+    cout<<"Enter no.of bro"<<endl;
+    cin>>bro_num;
+    if(bro_num==0){
+        cout<<"bat ban jayegi"<<endl;
+    }else{
+        cout<<"bat nahi banegi"<<endl;
+    }
+    return 0;
+}
